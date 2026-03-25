08ac6495954d4b8882735a150ca823e16ee48400
suvarnaks1 <suvarnaks2002@gmail.com>
2026-03-25 15:09:47 +0530

    fire

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:webprofile/about_me.dart';
 import 'package:webprofile/contact_page.dart';
+import 'package:webprofile/firebase_options.dart';
 import 'package:webprofile/hero_section.dart';
-import 'package:webprofile/nav_bar.dart';
 import 'package:webprofile/proect_section.dart';
 import 'package:webprofile/service_card.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
